c3b250081578ba124cc6bdc8ba55d54fbd512576
ImagineImogen <lieschen.yakov@gmail.com>
2016-11-11 18:39:05 +0200

    second attempt

---
+++ hello.py
@@ -0,0 +1,6 @@
+def wsgy_app (environ, start_response):
+    data=''
+    for i in environ["QUERY_STRING"].split('&'):
+        data = data+i+'\n'
+    start_response('200 OK',[('Content-Type', 'text/plain')])
+    return data
